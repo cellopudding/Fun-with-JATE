88a77521ccd2f95bc6a1aa18de35dd1967a6ceab
Lonnie Root <root.lonnie@gmail.com>
2023-05-01 17:59:46 -0400

    last pushes

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,9 +1,9 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
-const { ExpirationPlugin } = require('workbox-expiration');
+
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
@@ -38,10 +38,7 @@ new StaleWhileRevalidate({
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
-    new ExpirationPlugin({
-      maxEntries: 60,
-      maxAgeSeconds: 30 * 24 * 60 * 60, //30 Days
-    })
+   
   ],
 })
 );
--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -15,20 +15,11 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   
-console.error('putDb not implemented');
-// Create a connection to the database database and version we want to use.
+
 const jateDb = await openDB('jate', 1);
-  
-// Create a new transaction and specify the database and data privileges.
 const tx = jateDb.transaction('jate', 'readwrite');
-
-// Open up the desired object store.
 const store = tx.objectStore('jate');
-
-// Use the .add() method on the store and pass in the content.
 const request = store.put({ id: 1, value: content });
-
-// Get confirmation of the request.
 const result = await request;
 console.log('Successfully saved to database!', result);
 };
@@ -37,21 +28,13 @@ console.log('Successfully saved to database!', result);
 export const getDb = async () => {
   
   console.error('getDb not implemented');
-// Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
-
-  // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction('jate', 'readonly');
-
-  // Open up the desired object store.
   const store = tx.objectStore('jate');
-
-  // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
-
-  // Get confirmation of the request.
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  result ? console.log("data retrieved from database", result.value):console.log("data not found in the database")
+  return result?.value;
 };
 initdb();
